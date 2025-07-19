6236ed80e3662b240cd0b2fbe9d03c498fb0fac2
gos06065 <gos06065@knou.ac.kr>
2025-07-19 15:27:04 +0900

    비밀번호 찾기 결과 페이지 토글 추가

--- ecommerce-user/src/components/FindIdResult.jsx
+++ ecommerce-user/src/components/FindIdResult.jsx
@@ -1,18 +1,35 @@
 import { useState } from "react";
 import "./FindIdResult.css";
+import FindIdTypePw from "../components/FindIdTypePw"
 
 function FindIdResult({ idResult }) {
+    const [selected, setSelected] = useState('option1');
+
     return (
-        <><div>
-            <h1 className="find-id-title">eBear</h1>
-        </div>
-            <div className="id-result-area">
-                <p className="id-result">{idResult}</p>
+        <div className="find-id-container">
+            <div>
+                <h1 className="find-id-title">eBear</h1>
             </div>
-            <div className="buttons">
-                <button className="login-button">로그인</button>
-                <button className="find-pw-button">비밀번호 찾기</button>
-            </div></>
+            <div className="toggle-tab-container">
+                <div className={`toggle-tab ${selected === "option1" ? "active" : ""}`} onClick={() => setSelected("option1")}>
+                    아이디 찾기
+                </div>
+                <div className={`toggle-tab ${selected === "option2" ? "active" : ""}`} onClick={() => setSelected("option2")}>
+                    비밀번호 찾기
+                </div>
+            </div>
+
+            {
+                selected === 'option1' ?
+                    <><div className="id-result-area">
+                        <p className="id-result">{idResult}</p>
+                    </div>
+                        <div className="buttons">
+                            <button className="login-button">로그인</button>
+                            <button className="find-pw-button">비밀번호 찾기</button>
+                        </div></> : <FindIdTypePw />
+            }
+        </div>
     )
 }
 
