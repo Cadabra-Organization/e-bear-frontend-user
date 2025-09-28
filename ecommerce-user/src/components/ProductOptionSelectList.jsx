afe6f404832ccbf7ad3fd1a028759406701366df
Developer-Choi-Jae-Young <77819208+Developer-Choi-Jae-Young@users.noreply.github.com>
2025-09-28 15:42:00 +0900

    상품상세보기 페이지 컴포넌트 작업 1차

---
+++ ecommerce-user/src/components/ProductOptionSelectList.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './ProductOptionSelectList.css';
+
+function ProductOptionSelectList({selectProductOptionList}) {
+  
+    return (
+        <div className="product-options-list"> 
+            {selectProductOptionList.map((data, index) => {
+                return(
+                    <ul className="option_bottom ui_optSelWrapper  b_product_buy_selected">
+                        <li className="option_item c_product_option_item">
+                            <div className="option_item_info cont">
+                                <strong className="option_name">{data.optionSubject}</strong>
+                                <div className="option_amount c_product_input c_product_input_count">
+                                    <div className="amount-area">
+                                        <button type="button" className="btn_decrs on c_product_btn c_product_btn_count1"><span className="skip">수량감소</span></button>
+                                        <input type="text" name="prdcAmount" className="text form_input" title="수량설정" value={data.optionCount}/>
+                                        <button type="button" className="btn_incrs on c_product_btn c_product_btn_count2"><span className="skip">수량증가</span></button>
+                                    </div>
+                                </div>
+                                <dl className="c_prd_price">
+                                    <div className="price">
+                                        <dt>판매가</dt>
+                                        <dd><span className="value">{data.optionCount * data.optionPrice}</span><span className="unit">원</span></dd>
+                                    </div>
+                                </dl>
+                            </div>
+                            <div className="ctrl">
+                                <button type="button" className="btn_del c_product_btn c_product_btn_delete"></button>
+                            </div>
+                        </li>
+                    </ul>
+                )
+            })}
+        </div>
+    );
+}
+
+export default ProductOptionSelectList;
