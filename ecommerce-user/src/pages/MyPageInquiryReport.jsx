0702f1764cb130bc0c733456d5893a137ae2676e
gos06065 <gos06065@knou.ac.kr>
2025-11-09 15:15:27 +0900

    마이페이지 신고문의 css 정리

--- ecommerce-user/src/pages/MyPageInquiryReport.jsx
+++ ecommerce-user/src/pages/MyPageInquiryReport.jsx
@@ -1,41 +1,30 @@
-"use client"
-
-import { useState } from "react"
 import "./MyPageInquiryReport.css"
-import Headers from "../components/Headers"
-import Footer from "../components/Footer"
-import Navigation from "../components/Navigation"
 import SideNavigation from "../components/SideNavigation"
 import InquiryItem from "../components/InquiryItem"
 
 const MyPageInquiryReport = () => {
   let item = [
     {
-      imageUrl: "https://images.unsplash.com/photo-1549298916-b41d501d3772?w=100&h=100&fit=crop&crop=center" ,
-      brand: "asdasd" ,
-      productName: "asdasd" ,
-      title: "asdasd" ,
-      date: "asdasd" ,
-      content: "asd",
-
-      response: {
-        greeting: "으아ㅏㄱ",
-        content: "asdasd",
-        closing: "asdasd"
-      }
+      imageUrl: "https://images.unsplash.com/photo-1549298916-b41d501d3772?w=100&h=100&fit=crop&crop=center",
+      brand: "유닉스",
+      productName: "오브제 헤어 드라이기 UN-B1919N",
+      title: "유닉스 상품 재신고합니다",
+      content: "안녕하세요. 저는 노란색이 좋아서 노란색으로 사고 싶은데, 분홍색만 판다고 해서 신고합니다. 조치해 주세요.",
+      date: "2025.03.15",
+      replyStatus: 0
     },
     {
-      imageUrl: "asd" ,
-      brand: "asdasd" ,
-      productName: "asdasd" ,
-      title: "asdasd" ,
-      date: "asdasd" ,
-      content: "asd",
-
+      imageUrl: "https://images.unsplash.com/photo-1549298916-b41d501d3772?w=100&h=100&fit=crop&crop=center",
+      brand: "유닉스",
+      productName: "오브제 헤어 드라이기 UN-B1919N",
+      title: "유닉스 상품 신고합니다",
+      content: "안녕하세요. 저는 노란색이 좋아서 노란색으로 사고 싶은데, 분홍색만 판다고 해서 신고합니다. 조치해 주세요.",
+      date: "2025.02.13",
+      replyStatus: 1,
       response: {
-        greeting: "으아ㅏㄱ",
-        content: "asdasd",
-        closing: "asdasd"
+        greeting: "안녕하십니까, eBear입니다.",
+        content: "판매 제품의 색상, 수량 등에 관한 사항은 판매자 재량 영역으로, eBear 자체에서 강제할 수 없는 점 양해 부탁드립니다.",
+        closing: "앞으로도 eBear에 많은 관심 부탁드립니다. 감사합니다."
       }
     }
   ]
@@ -101,7 +90,6 @@ const MyPageInquiryReport = () => {
               {item.map((data, index) => (
                 <InquiryItem key={data.id || index} item={data} />
               ))}
-            {/* 두 번째 문의 */}
           </div>
         </main>
       </div>
