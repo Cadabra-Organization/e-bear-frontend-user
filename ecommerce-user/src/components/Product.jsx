d9d5e6d0b25789f48f1976674deca92d42a52ce1
Developer-Choi-Jae-Young <77819208+Developer-Choi-Jae-Young@users.noreply.github.com>
2025-11-16 16:05:00 +0900

    상품 카드 컴포넌트 작업

---
+++ ecommerce-user/src/components/Product.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Product.css';
+
+function Product({product}) {
+    const formatPrice = (price) => price.toLocaleString('ko-KR');
+    
+    return (
+        <div key={product.id} className="product-card">
+            <div className="product-image-container">
+                <img src={product.imageUrl || 'https://via.placeholder.com/300'} alt={product.name} />
+            </div>
+            <div className="product-details">
+                <span className="product-brand">{product.brand}</span>
+                <p className="product-name">{product.name}</p>
+                <div className="product-price">
+                    {/* 세일 퍼센트 존재할경우 세일가로 표시 */}
+                    {product.salePercentage ? (
+                        <div className="product-price">
+                            <span className="original-price">{formatPrice(product.price)}원</span>
+                            <span className="sale-info">
+                                <span className="sale-percentage">{product.salePercentage}%</span>
+                                <span className="sale-price">{formatPrice(product.price - ((product.price * product.salePercentage) / 100))}원</span>
+                            </span>
+                        </div>
+                    ) : (
+                        <div className="product-price">
+                            <span className="normal-price">{formatPrice(product.price)}원</span>
+                        </div>
+                    )}
+                    {/* 별점 표시 */}
+                    {product.rating && (
+                        <div className="product-rating-container">
+                            <span className="product-rating">★ {product.rating.toFixed(1)} ({product.reviewCount})</span>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Product;
