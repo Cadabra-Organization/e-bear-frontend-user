a4a44f0e4b34febee548af9b914baa9b297d2078
Developer-Choi-Jae-Young <77819208+Developer-Choi-Jae-Young@users.noreply.github.com>
2025-11-16 14:05:39 +0900

    메인 페이지 1차 작업

--- ecommerce-user/src/pages/MainPage.jsx
+++ ecommerce-user/src/pages/MainPage.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import "./MainPage.css";
 import Navigation from "../components/Navigation";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import memberBoon from '../../public/memberBoon.png';
 
 const MainPage = () => {
     let navigationMenu = [
@@ -25,11 +29,58 @@ const MainPage = () => {
             link: "/my-page/inquiry",
         }
     ]
+
+    const mainBannerList = [
+        {
+            name: "mainBanner1",
+            image: '/images/mainBanner1.png',
+            description: "",
+        },
+        {
+            name: "mainBanner2",
+            image: '/images/mainBanner1.png',
+            description: "",
+        },
+    ];
     
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+
     return (
         <>
             <Navigation navigationMenu={navigationMenu} />
-            메인페이지 입니다.
+
+            <Slider {...settings}>
+            {mainBannerList.map(item => 
+                <div className="image-container">
+                    <img src={item.image} alt={item.name}/>
+                </div>
+            )}
+            </Slider>
+
+            <div className="product-container">
+                <div className="sale-container">
+                    <span>오늘의 특가</span>
+
+                </div>
+                <div className="hot-container">
+                    <span>가장 많이 팔리는 물품</span>
+
+                </div>
+            </div>
+
+            <div className="member-boon-container">
+                <img src={memberBoon} width='100%' />
+            </div>
+
+            <div className="review-container">
+                
+            </div>
         </>
     )
 }
